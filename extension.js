38758c6414f3775e56d3c7d035ebd67ff0b30a50
Eric Swanson <es20641@gmail.com>
2025-03-20 17:04:59 -0500

    try fixing GType error

--- extension.js
+++ extension.js
@@ -1,67 +1,57 @@
 import GLib from "gi://GLib";
 import St from "gi://St";
 import Gio from "gi://Gio";
-import Clutter from "gi://Clutter";
 
+import { Extension } from "resource:///org/gnome/shell/extensions/extension.js";
 import * as Main from "resource:///org/gnome/shell/ui/main.js";
 import * as PanelMenu from "resource:///org/gnome/shell/ui/panelMenu.js";
 import * as PopupMenu from "resource:///org/gnome/shell/ui/popupMenu.js";
 
-import { Extension } from "resource:///org/gnome/shell/extensions/extension.js";
+export default class BibleVerseExtension extends Extension {
+  enable() {
+    // Create the button
+    this._indicator = new PanelMenu.Button(0.0, "Bible Verse Indicator", true);
 
-class BibleVerseIndicator extends PanelMenu.Button {
-  constructor(extension) {
-    // Use 0.0 instead of 0 as the alignment
-    super(0.0, "Bible Verse Indicator", false);
-
-    this._extension = extension;
-
-    // Icon for the panel
-    this.icon = new St.Icon({
-      icon_name: "document-edit-symbolic", // Using a standard icon that should exist
+    // Add icon
+    let icon = new St.Icon({
+      icon_name: "document-edit-symbolic",
       style_class: "system-status-icon"
     });
+    this._indicator.add_child(icon);
 
-    // Add icon to the panel
-    this.add_child(this.icon);
+    // Create menu items
+    this._verseItem = new PopupMenu.PopupMenuItem("Loading verse...");
+    this._verseItem.sensitive = false;
+    this._indicator.menu.addMenuItem(this._verseItem);
 
-    // Create menu item for the verse
-    this._menuItem = new PopupMenu.PopupMenuItem("Loading verse...");
-    this._menuItem.sensitive = false; // Not clickable
-    this.menu.addMenuItem(this._menuItem);
-
-    // Reference item that shows the verse reference
     this._referenceItem = new PopupMenu.PopupMenuItem("");
     this._referenceItem.sensitive = false;
     this._referenceItem.label.set_style("font-style: italic;");
-    this.menu.addMenuItem(this._referenceItem);
+    this._indicator.menu.addMenuItem(this._referenceItem);
+
+    // Add button to the panel
+    Main.panel.addToStatusArea("bible-verse-indicator", this._indicator);
 
     // Update verse when menu opens
-    this.menu.connect("open-state-changed", (menu, isOpen) => {
-      if (isOpen) {
-        this._updateVerse();
-      }
+    this._indicator.menu.connect("open-state-changed", (menu, isOpen) => {
+      if (isOpen) this._updateVerse();
     });
 
-    // Initial verse load
+    // Update initially
     this._updateVerse();
 
-    // Update verse every 5 minutes
-    this._updateTimeout = GLib.timeout_add_seconds(
-      GLib.PRIORITY_DEFAULT,
-      300, // 5 minutes
-      () => {
-        this._updateVerse();
-        return GLib.SOURCE_CONTINUE;
-      }
-    );
+    // Set up periodic updates
+    this._timeout = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 300, () => {
+      this._updateVerse();
+      return GLib.SOURCE_CONTINUE;
+    });
   }
 
   _updateVerse() {
     try {
       // Get verses from file
       const versesFile = Gio.File.new_for_path(
-        GLib.build_filenamev([this._extension.path, "verses.txt"])
+        GLib.build_filenamev([this.path, "verses.txt"])
       );
 
       if (!versesFile.query_exists(null)) {
@@ -96,35 +86,28 @@ class BibleVerseIndicator extends PanelMenu.Button {
         this._setVerseText(verse, "");
       }
     } catch (e) {
-      console.error(`Bible Verse Error: ${e.message}`);
+      console.error(`Bible Verse Error: ${e}`);
       this._setVerseText("Error loading verse.", "");
     }
   }
 
   _setVerseText(text, reference) {
-    this._menuItem.label.text = text;
-    this._referenceItem.label.text = reference;
-  }
-
-  destroy() {
-    if (this._updateTimeout) {
-      GLib.source_remove(this._updateTimeout);
-      this._updateTimeout = null;
-    }
-    super.destroy();
-  }
-}
-
-export default class BibleVerseExtension extends Extension {
-  enable() {
-    this._indicator = new BibleVerseIndicator(this);
-    Main.panel.addToStatusArea("bible-verse-indicator", this._indicator);
+    if (this._verseItem) this._verseItem.label.text = text;
+    if (this._referenceItem) this._referenceItem.label.text = reference;
   }
 
   disable() {
+    if (this._timeout) {
+      GLib.source_remove(this._timeout);
+      this._timeout = null;
+    }
+
     if (this._indicator) {
       this._indicator.destroy();
       this._indicator = null;
     }
+
+    this._verseItem = null;
+    this._referenceItem = null;
   }
 }
